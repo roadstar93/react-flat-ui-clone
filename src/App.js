b21d3f80b7e0253388302dd8b2ed982f36f44dc1
Robert <roadstar93@gmail.com>
2019-05-27 19:15:03 +0300

    Start styling PaletteList withStyles

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         <Route
           exact
           path="/"
-          render={() => <PaletteList paletts={seedColors} />}
+          render={() => <PaletteList palettes={seedColors} />}
         />
         <Route
           exact
