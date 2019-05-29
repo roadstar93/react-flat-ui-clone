437fee04b70aac605ed9647d743473126c883c0d
Robert <roadstar93@gmail.com>
2019-05-30 00:52:00 +0300

    finish refactor of ColorBox css into JSS

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -8,7 +8,7 @@ import "./ColorBox.css";
 const styles = {
   ColorBox: {
     width: "20%",
-    height: props=> props.showingFullPalette ? "25%" : "50%",
+    height: props => (props.showingFullPalette ? "25%" : "50%"),
     margin: "0 auto",
     display: "inline-block",
     position: "relative",
@@ -60,6 +60,67 @@ const styles = {
     border: "none",
     textDecoration: "none",
     opacity: "0"
+  },
+  boxContent: {
+    position: "absolute",
+    padding: "10px",
+    left: "0px",
+    width: "100%",
+    bottom: "0px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px"
+  },
+  copyOverlay: {
+    opacity: "0",
+    zIndex: "0",
+    width: "100%",
+    height: "100%",
+    transform: "scale(0.1)",
+    transition: "transform 0.6s ease-in-out"
+  },
+  showOverlay: {
+    opacity: "1",
+    transform: "scale(50)",
+    zIndex: "10",
+    position: "absolute"
+  },
+  copyMsg: {
+    position: "fixed",
+    left: "0",
+    right: "0",
+    top: "0",
+    bottom: "0",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "4rem",
+    transform: "scale(0.1)",
+    opacity: "0",
+    color: "white",
+    "& h1": {
+      fontWeight: "400",
+      textShadow: "1px 2px black",
+      background: "rgba(255, 255, 255, 0.2)",
+      width: "100%",
+      textAlign: "center",
+      marginBottom: "0",
+      padding: "1rem",
+      textTransform: "uppercase"
+    },
+    "& p": {
+      fontSize: "2rem",
+      fontWeight: "100"
+    }
+  },
+  copyMsgShow: {
+    opacity: "1",
+    transform: "scale(1)",
+    zIndex: "25",
+    transition: "all 0.5 ease-in-out",
+    transitionDelay: "0.3s"
   }
 };
 
@@ -75,7 +136,13 @@ class ColorBox extends Component {
     });
   }
   render() {
-    const { name, background, moreUrl, showingFullPalette, classes } = this.props;
+    const {
+      name,
+      background,
+      moreUrl,
+      showingFullPalette,
+      classes
+    } = this.props;
     const { copied } = this.state;
 
     return (
@@ -83,14 +150,17 @@ class ColorBox extends Component {
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`copy-overlay ${copied && "show"}`}
+            className={`${classes.copyOverlay} ${copied &&
+              classes.showOverlay}`}
           />
-          <div className={`copy-msg ${copied && "show"}`}>
+          <div
+            className={`${classes.copyMsg} ${copied && classes.copyMsgShow}`}
+          >
             <h1 className={classes.copyText}>Copied</h1>
             <p className={classes.copyText}>{this.props.background}</p>
           </div>
-          <div className="copy-container">
-            <div className="box-content">
+          <div>
+            <div className={classes.boxContent}>
               <span className={classes.colorName}>{name}</span>
             </div>
             <button className={classes.copyButton}>Copy</button>
