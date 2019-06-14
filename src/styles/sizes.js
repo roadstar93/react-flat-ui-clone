db1e65726c5b5d5fbb68bbf6a92667d4290376b1
roadstar93 <roadstar93@gmail.com>
2019-06-14 23:10:21 +0300

    Make palette list responsive

--- src/styles/sizes.js
+++ src/styles/sizes.js
@@ -1,15 +1,14 @@
 export default {
-    up(){
+  up() {},
 
-    },
-
-    down(size){
-        const sizes = {
-            xs: "576px",
-            sm: "768px",
-            md:"992px",
-            lg:"1200px"
-        }
-        return `@media (max-width: ${sizes[size]})`
-    }
-}
+  down(size) {
+    const sizes = {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1600px"
+    };
+    return `@media (max-width: ${sizes[size]})`;
+  }
+};
