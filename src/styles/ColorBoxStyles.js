6d26d3aeca308b2f9252ed410dd67aa63cad19e3
roadstar93 <roadstar93@gmail.com>
2019-06-14 21:52:43 +0300

    Make colorbox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,7 +15,7 @@ export default {
     },
     [sizes.down("lg")] :{
       width: "25%",
-      height: props => (props.showingFullPalette ? "20%" : "30%"),
+      height: props => (props.showingFullPalette ? "20%" : "33.3333%"),
     },
     [sizes.down("md")] :{
       width: "50%",
@@ -116,7 +116,10 @@ export default {
       textAlign: "center",
       marginBottom: "0",
       padding: "1rem",
-      textTransform: "uppercase"
+      textTransform: "uppercase",
+      [sizes.down("xs")] :{
+       fontSize: "5rem"
+      },
     },
     "& p": {
       fontSize: "2rem",
--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 export default {
     Palette: {
       height: "100vh",
@@ -38,6 +39,18 @@ export default {
         border: "none",
         textDecoration: "none",
         
+      },
+      [sizes.down("lg")] :{
+        width:"75%",
+        height:"33.3333%"
+      },
+      [sizes.down("md")] :{
+        width:"50%",
+        height:"20%"
+      },
+      [sizes.down("xs")] :{
+        width:"100%",
+        height:"10%"
       }
     }
   };
