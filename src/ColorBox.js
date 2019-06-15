e802f53c145afe5edc0788bba8fc261da9c82c21
roadstar93 <roadstar93@gmail.com>
2019-06-15 11:22:56 +0300

    Add classNames to ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,9 +2,8 @@ import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/styles";
-import styles from "./styles/ColorBoxStyles"
-
-
+import styles from "./styles/ColorBoxStyles";
+import classNames from "classnames";
 
 class ColorBox extends Component {
   constructor(props) {
@@ -32,12 +31,17 @@ class ColorBox extends Component {
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`${classes.copyOverlay} ${copied &&
-              classes.showOverlay}`}
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied
+            })}
           />
+
           <div
-            className={`${classes.copyMsg} ${copied && classes.copyMsgShow}`}
+            className={classNames(classes.copyMsg, {
+              [classes.copyMsgShow]: copied
+            })}
           >
+        
             <h1 className={classes.copyText}>Copied</h1>
             <p className={classes.copyText}>{this.props.background}</p>
           </div>
